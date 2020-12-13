beae607ced76fa58053886d8f9047067aa05b4ed
kalin balabnov <kalin_dude10@live.com>
2020-12-13 15:55:57 +0000

    Initial Commit

---
+++ Shared/WetherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WetherApp_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Kalin Balabanov on 13/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct WetherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
